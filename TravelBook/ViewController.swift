5a3734681b2cfc520e0aead2c5eb27cc1baf246f
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-05-22 21:33:46 +0500

    Created Map -Set up the user location

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -7,18 +7,85 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController {
+    
 
     @IBOutlet var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapDelegate()
+        managerLocation()
+        
+        
     }
-
+    
+    
+    
+    
 
 }
+
+
+//  MARK: - Extensions
+
+extension ViewController : MKMapViewDelegate {
+    
+    func mapDelegate() {
+        mapView.delegate = self
+    }
+    
+    
+    
+}
+
+extension ViewController : CLLocationManagerDelegate {
+    
+    
+    func managerLocation() {
+        
+        locationManager.delegate = self
+        
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    
+/*
+        locationManager function search the location of user and shows it on map
+ 
+ 
+ */
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+//        How much do i want to zoom in , in map
+//        How much the number smaller then zoom is out bigger
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+}
+
+
+
 
 
